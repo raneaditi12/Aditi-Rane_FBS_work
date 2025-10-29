852c3d9472aa1e62a407ab8aa946360abf573e39
raneaditi12 <139848215+raneaditi12@users.noreply.github.com>
2025-10-30 00:05:42 +0530

    Add files via upload

---
+++ c programming/tests/test 2/test2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main() {
+	int unit=45;
+	int bill;
+
+	if(unit<=50) {
+
+		bill=30*unit;
+		printf("eletricity bill is %d",bill);
+	} else if(unit<=150) {
+		bill=40*unit;
+		printf("eletriity bill is:%d",bill);
+
+	} else {
+		bill=50*unit;
+		printf("eletriity bill is:%d",bill);
+	}
+
+}
---
+++ c programming/tests/test 2/test2q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int no=20;
+	if(no>0)
+	printf("no is positive");
+	else if(no<0)
+	printf("no is negative");
+	else
+	printf("no is neutral");
+	
+}
